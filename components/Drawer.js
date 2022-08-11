b2d962e38a50f9dceb82c07c1adead3e1f707ca1
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-08-11 10:16:11 +0400

    Finished the basic design for Drawer component

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,32 +1,35 @@
+import { Input, InputLeftElement, InputGroup, Button } from "@chakra-ui/react";
+import { MdBuild, MdSearch } from "react-icons/md";
+
 export default function Drawer() {
   return (
-    <div
-      className="drawer-container"
-      style={{
-        backgroundColor: "rgba(255, 255, 255, 0.72)",
-        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-        backdropFilter: "blur(5.7px)",
-        webkitBackdropFilter: "blur(5.7px)",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "25vw",
-        color: "#000",
-        borderTopLeftRadius: "2.5rem",
-        borderBottomLeftRadius: "2.5rem",
-      }}
-    >
+    <>
+      {" "}
       <div
-        className="drawer"
+        className="drawer-container glass-light"
         style={{
+          height: "100vh",
           display: "flex",
           flexDirection: "column",
+          justifyContent: "flex-start",
+          width: "25vw",
+          color: "#000",
+          borderTopLeftRadius: "1rem",
+          borderBottomLeftRadius: "1rem",
         }}
       >
-        <h1>Drawer</h1>
+        <div
+          className="drawer"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            padding: "2rem 4rem",
+          }}
+        >
+          <h2>Drawer (WIP)</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
