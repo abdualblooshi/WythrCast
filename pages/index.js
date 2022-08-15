c8b834adf85de260dff4646c1fd0558c332cf602
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-08-15 18:50:28 +0400

    Added 5 new background images for the rest of weather conditions

--- pages/index.js
+++ pages/index.js
@@ -25,109 +25,43 @@ import LoadingModal from "../components/LoadingModal";
 // Night Images
 import clearNight from "../public/images/clear_night.jpg";
 import cloudyNight from "../public/images/cloudy_night.jpg";
+import drizzleNight from "../public/images/drizzle_night.jpg";
+import dustNight from "../public/images/dust_night.jpg";
 
 // Day Images
 import clearDay from "../public/images/clear_day.jpg";
 import cloudyDay from "../public/images/clouds.jpg";
 import snowyDay from "../public/images/snow_day.jpg";
 import dustDay from "../public/images/dust_day.jpg";
+import drizzleDay from "../public/images/drizzle_day.jpg";
 
 // Weather Condition Images
 import hotWeather from "../public/images/clear_hot.jpg";
 import rainWeather from "../public/images/rain.jpg";
 import stormWeather from "../public/images/thunderstorm.jpg";
 import mistWeather from "../public/images/mist.jpg";
+import tornadoWeather from "../public/images/tornado.jpg";
+import volcanoWeather from "../public/images/volcano.jpg";
+
 import { data } from "autoprefixer";
 import { render } from "react-dom";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-const getBackgroundImage = (sunrise, sunset, data, time) => {
-  const targetTime = new Date(time * 1000);
-  const currentHours = targetTime.getHours();
-  const currentMinutes = targetTime.getMinutes();
-  const sunriseTime = new Date(sunrise * 1000);
-  const sunriseHours = sunriseTime.getHours();
-  const sunriseMinutes = sunriseTime.getMinutes();
-  const sunsetTime = new Date(sunset * 1000);
-  const sunsetHours = sunsetTime.getHours();
-  const sunsetMinutes = sunsetTime.getMinutes();
-
-  const current_time = {
-    hours: currentHours,
-    minutes: currentMinutes,
-  };
-
-  const sunrise_time = {
-    hours: sunriseHours,
-    minutes: sunsetMinutes,
-  };
-
-  const sunset_time = {
-    hours: sunsetHours,
-    minutes: sunsetMinutes,
-  };
-
-  if (
-    current_time.hours >= sunrise_time.hours &&
-    current_time.minutes >= sunrise_time.minutes &&
-    current_time.hours <= sunset_time.hours &&
-    current_time.minutes <= sunset_time.minutes
-  ) {
-    // Day Pictures
-    if (data.weather[0].main === "Clear") {
-      return clearDay.src;
-    } else if (data.weather[0].main === "Clouds") {
-      return cloudyDay.src;
-    } else if (
-      (data.weather[0].main === "Rain" &&
-        data.weather[0].description === "light rain") ||
-      data.weather[0].description === "moderate rain"
-    ) {
-      return rainWeather.src;
-    } else if (
-      data.weather[0].main === "Rain" &&
-      data.weather[0].description === "heavy intensity rain"
-    ) {
-      return stormWeather.src;
-    } else if (data.weather[0].main === "Mist") {
-      return mistWeather.src;
-    } else if (data.weather[0].main === "Dust") {
-      return dustDay.src;
-    } else if (data.main.temp > 30) {
-      return hotWeather.src;
-    } else if (data.main.temp < 0) {
-      return snowyDay.src;
-    }
-  } else {
-    // Night Pictures
-    if (data.weather[0].main === "Clear") {
-      return clearNight.src;
-    } else if (data.weather[0].main === "Rain") {
-      return rainWeather.src;
-    } else if (data.weather[0].main === "Mist") {
-      return mistWeather.src;
-    } else if (data.weather[0].main === "Clouds") {
-      return cloudyNight.src;
-    } else if (data.weather[0].main === "Dust") {
-      return dustDay.src;
-    }
-  }
-};
 
 export default function Home(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [searchValue, setSearchValue] = useState("");
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
-  const [timeZone, setTimeZone] = useState(props.timeZone);
-  const [time, setTime] = useState(Date.now());
+  const [time, setTime] = useState("");
+  const [timeYMD, setTimeYMD] = useState("");
   const [error, setError] = useState(false);
   const [isLoading, setLoading] = useState(true);
   const [sunrise, setSunrise] = useState(0);
   const [sunset, setSunset] = useState(0);
   const [backgroundImage, setBackground] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [date, setDate] = useState(new Date().toLocaleDateString());
 
   useEffect(() => {
     const fetchData = async () => {
@@ -139,33 +73,127 @@ export default function Home(props) {
 
       http.setMaxRPS(1);
 
+      const changeBackground = (sunrise, sunset, data, targetTime) => {
+        console.log("Current Time in City: ", targetTime);
+        const currentHours = targetTime.getHours();
+        const currentMinutes = targetTime.getMinutes();
+        const sunriseTime = new Date(sunrise * 1000);
+        console.log("Sunrise Time in City: ", sunriseTime);
+        const sunriseHours = sunriseTime.getHours();
+        const sunriseMinutes = sunriseTime.getMinutes();
+        const sunsetTime = new Date(sunset * 1000);
+        console.log("Sunset Time in City: ", sunsetTime);
+        const sunsetHours = sunsetTime.getHours();
+        const sunsetMinutes = sunsetTime.getMinutes();
+
+        const current_time = {
+          hours: currentHours,
+          minutes: currentMinutes,
+        };
+
+        const sunrise_time = {
+          hours: sunriseHours,
+          minutes: sunriseMinutes,
+        };
+
+        const sunset_time = {
+          hours: sunsetHours,
+          minutes: sunsetMinutes,
+        };
+
+        if (
+          current_time.hours >= sunrise_time.hours &&
+          current_time.hours <= sunset_time.hours
+        ) {
+          // Day Pictures
+          if (data.weather[0].main === "Clear") {
+            setBackground(clearDay.src);
+          } else if (data.weather[0].main === "Clouds") {
+            setBackground(cloudyDay.src);
+          } else if (
+            (data.weather[0].main === "Rain" &&
+              data.weather[0].description === "light rain") ||
+            data.weather[0].description === "moderate rain"
+          ) {
+            setBackground(rainWeather.src);
+          } else if (
+            data.weather[0].main === "Rain" &&
+            data.weather[0].description === "heavy intensity rain"
+          ) {
+            setBackground(stormWeather.src);
+          } else if (data.weather[0].main === "Drizzle") {
+            setBackground(drizzleDay.src);
+          } else if (
+            data.weather[0].main === "Mist" ||
+            data.weather[0].main === "Haze" ||
+            data.weather[0].main === "Fog"
+          ) {
+            setBackground(mistWeather.src);
+          } else if (
+            data.weather[0].main === "Dust" ||
+            data.weather[0].main === "Sand"
+          ) {
+            setBackground(dustDay.src);
+          } else if (data.main.temp > 30) {
+            setBackground(hotWeather.src);
+          } else if (data.main.temp < 0) {
+            setBackground(snowyDay.src);
+          } else if (
+            data.weather[0].main === "Tornado" ||
+            data.weather[0].main === "Squall"
+          ) {
+            setBackground(tornadoWeather.src);
+          } else if (data.weather[0].main === "Volcano") {
+            setBackground(volcanoWeather.src);
+          }
+        } else {
+          // Night Pictures
+          if (data.weather[0].main === "Clear") {
+            setBackground(clearNight.src);
+          } else if (data.weather[0].main === "Rain") {
+            setBackground(rainWeather.src);
+          } else if (
+            data.weather[0].main === "Mist" ||
+            data.weather[0].main === "Haze" ||
+            data.weather[0].main === "Fog"
+          ) {
+            setBackground(mistWeather.src);
+          } else if (data.weather[0].main === "Clouds") {
+            setBackground(cloudyNight.src);
+          } else if (data.weather[0].main === "Dust") {
+            setBackground(dustNight.src);
+          } else if (data.weather[0].main === "Snow") {
+            setBackground(snowyDay.src);
+          } else if (data.weather[0].main === "Drizzle") {
+            setBackground(drizzleNight.src);
+          } else if (
+            data.weather[0].main === "Tornado" ||
+            data.weather[0].main === "Squall"
+          ) {
+            setBackground(tornadoWeather.src);
+          } else if (data.weather[0].main === "Volcano") {
+            setBackground(volcanoWeather.src);
+          }
+        }
+      };
+
       setLoading(true);
       try {
-        const ipRequest = await http.get(
-          `http://ip-api.com/json/${props.ip}?fields=status,message,country,city,lat,lon,query,timezone`,
+        const timeRequest = await http.get(
+          `https://api.ipgeolocation.io/timezone?apiKey=${process.env.TIMEZONE_API_KEY}&location=${city}`,
           {
             cache: {
-              maxAge: 2 * 60 * 1000, // 2 min instead of 15 min
+              maxAge: 15 * 60 * 1000, // 2 min instead of 15 min
               exclude: { query: false },
             },
           }
         );
-        const ipResponse = ipRequest.data;
 
-        const timeRequest = await axios.get(
-          `http://worldtimeapi.org/api/timezone/${ipResponse.timezone}`,
-          {
-            cache: {
-              maxAge: 2 * 60 * 1000, // 2 min instead of 15 min
-              exclude: { query: false },
-            },
-          }
-        );
         const weatherRequest = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${process.env.WEATHER_API_KEY}`,
           {
             cache: {
-              maxAge: 2 * 60 * 1000, // 2 min instead of 15 min
+              maxAge: 15 * 60 * 1000, // 2 min instead of 15 min
               exclude: { query: false },
             },
           }
@@ -175,25 +203,26 @@ export default function Home(props) {
           axios.spread(async (...responses) => {
             const timeResponse = responses[1];
             const weatherResponse = responses[0];
-            console.log(weatherResponse.request.fromCache);
-            const unixTime = () => {
-              return Math.floor(
-                new Date(timeResponse.data.datetime).getTime() / 1000
-              );
-            };
-            setTime(unixTime);
+            setTime(timeResponse.data.time_24.substring(0, 5));
+            setTimeYMD(timeResponse.data.date_time_ymd);
+            setDate(timeResponse.data.date);
             setWeather(weatherResponse.data);
             setSunrise(weatherResponse.data.sys.sunrise);
             setSunset(weatherResponse.data.sys.sunset);
-            console.log(time);
-            setBackground(
-              getBackgroundImage(
-                weatherResponse.data.sys.sunrise,
-                weatherResponse.data.sys.sunset,
-                weatherResponse.data,
-                time
-              )
+            changeBackground(
+              weatherResponse.data.sys.sunrise,
+              weatherResponse.data.sys.sunset,
+              weatherResponse.data,
+              new Date(timeResponse.data.date_time_ymd) // <--- to be fixed
+              /*
+               * TODO:
+               * new Date() won't work because it keeps referencing back to the same time value,
+               * it has to be changed to a string that contains time in 24-hour format
+               * not a YMD format datetime string
+               */
             );
+            console.log(city);
+            console.log("-------------");
             setError(false);
             setLoading(false);
           })
@@ -202,7 +231,7 @@ export default function Home(props) {
         console.log(error);
         if (error.request.status === 404) {
           setError(true);
-          setErrorMessage("Wrong city/country name!");
+          setErrorMessage("Wrong city/country!");
           setLoading(false);
         } else {
           setErrorMessage("Error!");
@@ -213,7 +242,6 @@ export default function Home(props) {
     };
     fetchData();
   }, [city]);
-  console.log(weather);
   return (
     <>
       <Head>
@@ -264,7 +292,12 @@ export default function Home(props) {
             height: "100vh",
           }}
         >
-          <Weather weather={weather} time={time} isLoading={isLoading} />}
+          <Weather
+            weather={weather}
+            time={time}
+            isLoading={isLoading}
+            date={date}
+          />
           <Drawer
             city={city}
             weather={weather}
@@ -289,7 +322,6 @@ export async function getServerSideProps(context) {
   );
   const defaultCity = cityRequest.data.city;
   const timeZone = cityRequest.data.timezone;
-  console.log(defaultCity);
 
   return {
     props: {
